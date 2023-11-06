9cf5467685f60b5cf4267f0459ddc9d7760b37bd
S_ent <asdf@gail.com>
2023-11-07 08:34:51 +0900

    Initial commit

---
+++ models/base.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime
+from sqlalchemy.orm import declarative_base
+
+from connect import session
+
+Model = declarative_base()
+Model.query = session.query_property() # User.query 사용 가능
+
+
+class TimeStampedModel(Model):
+    __abstract__ = True # 직접 객체로 만들 수 없고 상속받아야 함.
+
+    created_at = Column(DateTime, default=datetime.utcnow())
+    updated_at = Column(DateTime, onupdate=datetime.utcnow()) # 마지막 update값만 남음에 유의. 변경이력 관리 불가
+
+    # 한국 표준시
+    # dt_kst = datetime.datetime.utcnow() + datetime.timedelta(hours=9)
---
+++ models/tables.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, Date, Time, DateTime
+from sqlalchemy.orm import Relationship
+
+from models.base import TimeStampedModel, Model
+
+class User(TimeStampedModel):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(80), nullable=False)
+    email = Column(String(320), nullable=False, unique=True)
+    password = Column(String(320), nullable=False)
+    is_admin = Column(Boolean, nullable=False)
+
+    alarms = Relationship('Alarm', back_populates='user')
+
+class Alarm(TimeStampedModel):
+    __tablename__ = 'alarms'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'))
+    repeat_now = Column(Boolean, nullable=False)
+    repeat_week = Column(Integer, nullable=False)
+    alarm_date = Column(Date, nullable=False)
+    alarm_time = Column(Time, nullable=False)
+
+    user = Relationship('User', back_populates='alarms')
+    alarm_repeat = Relationship('Alarm_repeat', back_populates='alarm')
+
+class Alarm_repeat(TimeStampedModel):
+    __tablename__ = 'alarm_repeats'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    alarm_id = Column(Integer, ForeignKey('alarms.id', ondelete='CASCADE'))
+    repeat_interval = Column(Integer, nullable=False)
+    repeat_time = Column(DateTime, nullable=False)
+
+    alarm = Relationship('Alarm', back_populates='alarm_repeat')
